4c81ef5a5615f1c3bbae770294315bd1e531b1fd
Lucas Monteiro <lukasmonteir0@hotmail.com>
2021-02-03 18:28:54 -0300

    Add search page

--- src/pages/Search/index.tsx
+++ src/pages/Search/index.tsx
@@ -2,15 +2,23 @@ import Button from '../../core/components/Button'
 import './styles.css'
 
 const Search = () => {
+  const handleOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(event.target.value)
+  }
+
   return (
     <div className="search-container">
       <div className="search-content">
         <h1 className="search-title">Encontre um perfil Github</h1>
         
-        <input type="text"/>
+        <input
+          type="text"
+          className="search-input"
+          placeholder="Usuário Github"
+          onChange={ handleOnChange }
+        />
 
         <Button text="Encontrar" />
-
       </div>
     </div>
   )
