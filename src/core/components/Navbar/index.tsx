def8188445ca6946ab23b0af745bb3d111fa212d
Lucas Monteiro <lukasmonteir0@hotmail.com>
2021-02-03 15:45:04 -0300

    Add Navbar

---
+++ src/core/components/Navbar/index.tsx
@@ -0,0 +1,13 @@
+import './styles.css'
+
+const Navbar = () => {
+  return (
+    <nav className="nav-container">
+      <div className="nav-content">
+        <a className="nav-logo-text" href="#home">Bootcamp DevSuperior</a>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
