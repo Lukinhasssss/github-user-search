0bc1e7d04ff006fd0637702852c478a4eb1d39be
Lucas Monteiro <lukasmonteir0@hotmail.com>
2021-02-04 09:08:44 -0300

    Add loaders and finish search page

--- src/pages/Search/index.tsx
+++ src/pages/Search/index.tsx
@@ -1,9 +1,49 @@
+import { useState } from 'react'
+
 import Button from '../../core/components/Button'
+import { makeRequest } from '../../core/utils/requests'
+import ImageLoader from './components/Loaders/ImageLoader'
+import InfoLoader from './components/Loaders/InfoLoader'
+
 import './styles.css'
 
+type UserProps = {
+  name: string
+  avatar_url: string
+  html_url: string
+  public_repos: number
+  followers: number
+  following: number
+  company: string
+  blog: string
+  location: string
+  created_at: string
+}
+
 const Search = () => {
+  const [githubResponse, setGithubResponse] = useState<UserProps>()
+  const [isLoading, setIsLoading] = useState(false)
+  const [input, setInput] = useState('')
+  const [showSearchResult, setShowSearchResult] = useState(false)
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+
+    setIsLoading(true)
+
+    makeRequest({ url: `/${ input }` })
+      .then(response => setGithubResponse(response.data))
+      .finally(() => {
+        setIsLoading(false)
+    })
+
+    setShowSearchResult(true)
+  }
+
   const handleOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    console.log(event.target.value)
+    const value = event.target.value
+
+    setInput(value)
   }
 
   return (
@@ -11,42 +51,53 @@ const Search = () => {
       <div className="search-content">
         <h1 className="search-title">Encontre um perfil Github</h1>
         
-        <input
-          type="text"
-          className="search-input"
-          placeholder="Usuário Github"
-          onChange={ handleOnChange }
-        />
+        <form onSubmit={ handleSubmit }>
+          <input
+            type="text"
+            value={ input }
+            placeholder="Usuário Github"
+            className="search-input"
+            onChange={ handleOnChange }
+          />
 
-        <Button text="Encontrar" />
+          <div className="button-find">
+            <Button text="Encontrar" />
+          </div>
+        </form>
       </div>
 
-      <div className="search-result-content">
-        <div className="search-result-perfil">
-          <img src="https://avatars.githubusercontent.com/u/60019382?s=460&u=b336f565f0efc9a7a666c929458b0715b84f4c19&v=4" alt="Lucas Monteiro" className="search-result-perfil-img"/>
+      { showSearchResult && (
+        <div className="search-result-content">
+          {isLoading ? <ImageLoader /> : (
+            <div className="search-result-perfil">
+              <img src={ githubResponse?.avatar_url } alt={ githubResponse?.name } className="search-result-perfil-img"/>
 
-          <a href="https://github.com/Lukinhasssss" target="_blank" rel="noopener noreferrer">
-            <Button text="Ver perfil" />
-          </a>
+              <a href={ githubResponse?.html_url } target="_blank" rel="noopener noreferrer">
+                <Button text="Ver perfil" />
+              </a>
+            </div>
+          )}
+
+          {isLoading ? <InfoLoader /> : (
+            <div className="search-result-info">
+              <div className="github-info">
+                <span>Repositórios públicos: { githubResponse?.public_repos }</span>
+                <span>Seguidores: { githubResponse?.followers }</span>
+                <span>Seguindo: { githubResponse?.following }</span>
+              </div>
+
+              <div className="perfil-info">
+                <h1>Informações</h1>
+
+                <p><strong>Empresa:</strong> { githubResponse?.company }</p>
+                <p><strong>Website/Blog:</strong> { githubResponse?.blog }</p>
+                <p><strong>Localidade:</strong> { githubResponse?.location }</p>
+                <p><strong>Membro desde:</strong> { githubResponse?.created_at }</p>
+              </div>
+            </div>
+          )}
         </div>
-
-        <div className="search-result-info">
-          <div className="github-info">
-            <span>Repositórios públicos: 18</span>
-            <span>Seguidores: 5</span>
-            <span>Seguindo: 11</span>
-          </div>
-
-          <div className="perfil-info">
-            <h1>Informações</h1>
-
-            <p><strong>Empresa:</strong> @Nubank</p>
-            <p><strong>Website/Blog:</strong> https://github.com/Lukinhasssss</p>
-            <p><strong>Localidade:</strong> São Bernardo do Campo - SP</p>
-            <p><strong>Membro desde:</strong> 17/01/2020</p>
-          </div>
-        </div>
-      </div>
+      )}
     </div>
   )
 }
